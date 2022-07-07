19e6b7ab7cc8bebd42e5c1f989814f9c2d6689a6
Allana Karla <allanakarla1702@gmail.com>
2022-07-07 19:20:44 -0300

    :sparkles: feat: cria conexão entre app e routerProducts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import routerProducts from './routers/products';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(routerProducts);
 
 export default app;
