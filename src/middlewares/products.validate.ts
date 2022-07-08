78682208e2e0fca7af46ecee2ac1f506f267a75f
Allana Karla <allanakarla1702@gmail.com>
2022-07-08 19:48:17 -0300

    :sparkles: feat: cria productsValidateMiddleware

---
+++ src/middlewares/products.validate.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import * as Joi from 'joi';
+import HttpException from '../shared/http.exception';
+
+const productsValidate = Joi.object({
+  name: Joi.string().min(2).required(),
+  amount: Joi.string().min(2).required(),
+});
+
+const productsValidateMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  const { error } = productsValidate.validate(req.body);
+  if (error) {
+    const { message, type } = error.details[0];
+    const status = type === 'any.required' ? 400 : 422;
+    throw new HttpException(status, message);
+  }
+  next();
+};
+
+export default productsValidateMiddleware;
