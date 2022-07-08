5f59a5d219b64cf0e277707fbb356142fa08270d
Allana Karla <allanakarla1702@gmail.com>
2022-07-08 19:49:14 -0300

    :sparkles: feat: cria rota /products - método POST

--- src/routers/products.ts
+++ src/routers/products.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import productsController from '../controllers/products.controller';
+import productsValidateMiddleware from '../middlewares/products.validate';
 
 const router = express.Router();
 
 router.get('/products', productsController.getAllProductsController);
+
+router.post('/products', productsValidateMiddleware, productsController.addProductsController);
 
 export default router;
