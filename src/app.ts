0a738e5fc8b2e07c8657eef7224bf0b828bdd9fa
Allana Karla <allanakarla1702@gmail.com>
2022-07-08 19:45:02 -0300

    :sparkles: feat: conecta middleware de erro e app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import middlewareError from './middlewares/error';
 import routerProducts from './routers/products';
 
 const app = express();
@@ -6,5 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(routerProducts);
+
+app.use(middlewareError);
 
 export default app;
