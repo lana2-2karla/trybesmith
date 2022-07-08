90113e2dd8dff033fb5f2f2b70c2d0d57a842c15
Allana Karla <allanakarla1702@gmail.com>
2022-07-08 19:50:23 -0300

    :recycle: refactor: refatora intarface IProduct

--- src/intarfaces/products.intarface.ts
+++ src/intarfaces/products.intarface.ts
@@ -2,7 +2,7 @@ interface IProduct {
   id?: number;
   name: string;
   amount: string;
-  orderId: null | number;
+  orderId?: null | number;
 }
 
 // 1 barra(orderId) - consulta: https://oieduardorabelo.medium.com/typescript-entendendo-a-nota%C3%A7%C3%A3o-de-tipos-9e8c1c89ef62
