4b86a9f0fb9a74ea4c63bf695e8081a83b77ccce
Allana Karla <allanakarla1702@gmail.com>
2022-07-09 18:25:33 -0300

    :sparkles: feat: cria conexão entre app e routerUsers

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import 'express-async-errors';
 import middlewareError from './middlewares/error';
 import routerProducts from './routers/products';
+import routerUsers from './routers/users';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routerProducts);
+app.use(routerUsers);
 
 app.use(middlewareError);
 
